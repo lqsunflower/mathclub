86bfbf336511329419cdd7797bb633948346b14d
liqiu <963572964@qq.com>
2017-08-07 18:10:08 +0800

    点赞接口
    点赞接口

---
+++ src/main/java/com/mathclub/controller/SubjectController.java
@@ -0,0 +1,78 @@
+/**
+ * 文件名：SubjectController.java
+ * 创建日期： 2017年8月7日
+ * 作者：     richinfo
+ * Copyright (c) 2009-2017 邮箱开发室
+ * All rights reserved.
+ */
+package com.mathclub.controller;
+
+import org.apache.log4j.Logger;
+
+import com.jfinal.core.ActionKey;
+import com.jfinal.kit.Ret;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.mathclub.model.Subject;
+import com.mathclub.service.AdminService;
+import com.mathclub.service.SubjectService;
+
+/**
+ * 功能描述：
+ *
+ */
+public class SubjectController extends BaseController
+{
+
+    private static Logger log = Logger.getLogger(SubjectController.class);
+    public static final Subject subjectDao = new Subject().dao();
+    private AdminService service = new AdminService();
+    private SubjectService subjectService = new SubjectService();
+
+    /**
+     * 根据题目ID获取题目信息
+     */
+    @ActionKey("/subject:findById")
+    public void getSubjectInfo()
+    {
+        log.info("get subject info request subjectId ="
+            + getParaToInt("subjectId") + " keyId=" + getParaToInt("keyId"));
+        Subject subject = service.getSubjectInfo(getParaToInt("subjectId"),
+            getParaToInt("keyId"));
+        renderJson(subject);
+    }
+
+    /**
+     * 根据题目ID获取题目信息
+     */
+    @ActionKey("/subject:supportSubject")
+    public void supportSubject()
+    {
+        log.info("support subject info request userId="
+            + getParaToInt("userId") + " subjectId ="
+            + getParaToInt("subjectId") + " type=" + getParaToInt("type"));
+        int type = getParaToInt("type");
+        if (type == 1 || type == 2)
+        {
+            boolean result = subjectService.checkUserExists(
+                getParaToInt("userId"), getParaToInt("subjectId"));
+            if (result)
+            {
+                renderJson(Ret.fail("summary", "该用户已经点赞或点跪"));
+                return;
+            }
+        }
+        boolean res = subjectService.support(getParaToInt("userId"),
+            getParaToInt("subjectId"), getParaToInt("type"));
+        if (res)
+        {
+            renderJson(Ret.ok());
+        }
+        else
+        {
+            renderJson(Ret.fail("code", "-2"));
+        }
+
+    }
+
+}
---
+++ src/main/java/com/mathclub/service/SubjectService.java
@@ -0,0 +1,58 @@
+/**
+* 文件名：SubjectService.java
+* 创建日期： 2017年8月7日
+* 作者：     richinfo
+* Copyright (c) 2009-2017 邮箱开发室
+* All rights reserved.
+*/
+package com.mathclub.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
+import com.mathclub.model.Subject;
+
+/**
+ * 功能描述：
+ *
+ */
+public class SubjectService
+{
+
+    public static final Subject subjectDao = new Subject().dao();
+    
+    /**
+     * 检查用户是否已点赞或点跪
+     * @param paraToInt
+     * @param paraToInt2
+     * @return
+     */
+    public boolean checkUserExists(Integer userId, Integer subjectId)
+    {
+        //添加缓存
+        //subjectDao.findByCache(cacheName, key, sql);
+        boolean result = false;
+        Subject subject = subjectDao.findFirst("select * from support where userId=? and subjectId=?",userId,subjectId);
+        if(subject != null){
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean support(Integer userId, Integer subjectId,
+        Integer type)
+    {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("userId", userId);
+        map.put("subjectId", subjectId);
+        map.put("type", type);
+        map.put("createTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        Record record = new Record().setColumns(map);
+        return Db.save("support", "id", record);
+    }
+
+}
